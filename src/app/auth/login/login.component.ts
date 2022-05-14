9c19ccefc11a3080b8f890abcf81d820707ef275
nguyenanhhus <nguyenanh.binoo@gmail.com>
2022-05-15 03:07:20 +0700

    Log In

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { LoginRequestPayload } from './login.request.payload';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  loginRequestPayload!: LoginRequestPayload;
+  isError: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.loginRequestPayload = {
+      username: '',
+      password: '',
+    };
   }
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  login() {
+    this.isError = true;
+    this.loginRequestPayload.username = this.loginForm.get('username')?.value;
+    this.loginRequestPayload.password = this.loginForm.get('password')?.value;
+
+    this.authService.login(this.loginRequestPayload).subscribe((data) => {
+      console.log('Login successful');
+      this.isError = false;
+    });
+  }
 }
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SignupRequestPayload } from '../signup/signup-request.payload';
-import { Observable } from 'rxjs';
+import { LoginRequestPayload } from '../login/login.request.payload';
+import { map, Observable } from 'rxjs';
+import { LoginResponse } from '../login/login-response.payload';
+import { LocalStorageService } from 'ngx-webstorage';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private localStorage: LocalStorageService
+  ) {}
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
     return this.http.post(
@@ -15,5 +21,25 @@ export class AuthService {
       signupRequestPayload,
       { responseType: 'text' }
     );
+  }
+
+  login(loginRequestPayload: LoginRequestPayload): Observable<boolean> {
+    return this.http
+      .post<LoginResponse>(
+        'http://localhost:8080/api/auth/login',
+        loginRequestPayload
+      )
+      .pipe(
+        map((data) => {
+          this.localStorage.store(
+            'authenticationToken',
+            data.authenticationToken
+          );
+          this.localStorage.store('username', data.username);
+          this.localStorage.store('refreshToken', data.refreshToken);
+          this.localStorage.store('expiresAt', data.expiresAt);
+          return true;
+        })
+      );
   }
 }
